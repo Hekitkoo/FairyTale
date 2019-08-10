29c24cbc32c594867081fcfa9427038bd2d3a678
Nikita Sokurenko <hekitko0@gmail.com>
2019-08-10 18:18:57 +0300

    first commit

---
+++ FairyTale/Characters/Bunny.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyTale
+{
+    /// <summary>
+    ///  it's a bunny(not main character)
+    /// </summary>
+    class Bunny : AbstractStoryObject
+    {
+        public Bunny()
+        {
+            Name = "Зайчик побегайчик";
+            Size = 30;
+            HowMove = "прыг-прыг";
+
+        }
+        public override string Name { get; set; }
+        public override int Size { get; set; }
+        public override string HowMove { get; set; }
+    }
+}
---
+++ FairyTale/Characters/Mouse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyTale
+{
+    /// <summary>
+    /// Character (not main)
+    /// </summary>
+    class Mouse : AbstractStoryObject
+    {
+        public Mouse()
+        {
+            Name = "Мышка поскребушка";
+            Size = 10;
+        }
+        public override string Name { get; set; }
+        public override int Size { get; set; }
+
+
+    }
+}
