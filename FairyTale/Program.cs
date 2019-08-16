5e5bdfd0438a406416a199e5e52d220f4f1f1d86
Nikita Sokurenko <hekitko0@gmail.com>
2019-08-16 08:25:45 +0300

    [Task_1] Programm.cs implemented

--- FairyTale/Program.cs
+++ FairyTale/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace FairyTale
 {
+    /// <summary>
+    /// Main /:
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            // Story Characters
+            StoryManager _storyManager = new StoryManager();
+            IMainStoryObject _oldMan = new OldMan();
+            AbstractStoryObject _mouse = new Mouse();
+            AbstractStoryObject _frog = new Frog();
+            AbstractStoryObject _bunny = new Bunny();
+            AbstractStoryObject _wolf = new Wolf();
+            AbstractStoryObject _bear = new Bear();
+            // Story
+            try
+            {
+            _storyManager.DoAct0();
+            _storyManager.DoAct1(_oldMan);
+            _storyManager.DoRepitableAct(_mouse);
+            _storyManager.DoRepitableAct(_frog);
+            _storyManager.DoRepitableAct(_bunny);
+            _storyManager.DoRepitableAct(_wolf);
+            _storyManager.DoAct2(_bear);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+                Console.WriteLine(ex.Message);
+            }
+
+
         }
     }
 }
