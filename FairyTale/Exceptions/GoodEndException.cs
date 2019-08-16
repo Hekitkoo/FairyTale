211cec82770c63e1f69b3a0dd5bc653e0580100b
Nikita Sokurenko <hekitko0@gmail.com>
2019-08-16 08:35:58 +0300

    [Task_2] Custom Exceptions added

---
+++ FairyTale/Exceptions/GoodEndException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyTale.Exceptions
+{
+    class GoodEndException : Exception
+    {
+        public GoodEndException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ FairyTale/Exceptions/MainCharacterOverflowException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyTale.Characters
+{
+    class MainCharacterOverflowException : Exception
+    {
+
+        public MainCharacterOverflowException(string message) : base(message)
+        {
+        }
+    }
+}
