3a1ec41fb882721c5d6c11fca39aed1d4deabcb4
Nikita Sokurenko <hekitko0@gmail.com>
2019-08-16 08:43:56 +0300

    [Task_2] deleted unused directives

--- FairyTale/Characters/Abstracts/StoryObject.cs
+++ FairyTale/Characters/Abstracts/StoryObject.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FairyTale
 {
@@ -11,20 +7,20 @@ namespace FairyTale
     /// </summary>
     abstract class StoryObject
     {
-
         public abstract string Name { get; set; }
-
         public abstract int Size { get; set; }
-
         public virtual string HowMove { get; set; } = "бежит";
-
         public virtual void Action(IMainStoryObject mainStoryObject)
         {
-            if (mainStoryObject.Objects.Count == 0)
-                Console.WriteLine("Тут я буду жить.");
-            else
-                Console.WriteLine($"Кто, кто в объекте типа {(mainStoryObject as StoryObject).Name} живет?");
+            if (mainStoryObject.Objects.Count != 0)
+                if (mainStoryObject is StoryObject)
+                {
+                    Console.WriteLine($"Кто, кто в объекте типа {(mainStoryObject as StoryObject).Name} живет?");
+                }
+                else
+                {
+                    Console.WriteLine("Тут я буду жить.");
+                }
         }
-
     }
 }
